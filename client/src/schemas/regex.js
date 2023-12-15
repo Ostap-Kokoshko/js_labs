7c67e896854842b6f4feff719d0afe38a7c24727
Ostap-Kokoshko <ostap.kokoshko.ir.2022@lpnu.ua>
2023-12-15 16:02:39 +0200

    Add regex for email

--- client/src/schemas/regex.js
+++ client/src/schemas/regex.js
@@ -4,11 +4,13 @@ const firstNameRegex = /^[a-zA-Zа-яА-Я]+$/;
 const lastNameRegex = /^[a-zA-Zа-яА-Я]+$/;
 //eslint-disable-next-line
 const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{2,3}[-\s\.]?[0-9]{2,3}[-\s\.]?[0-9]{2,3}[-\s\.]?[0-9]{2,3}$/im;
+//eslint-disable-next-line
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
 export const checkoutSchema = yup.object().shape({
     firstName: yup.string().matches(firstNameRegex, { message: "First Name shouldn't contain any special characters!" }).required("Field is required!"),
     lastName: yup.string().matches(lastNameRegex, { message: "Last Name shouldn't contain any special characters!" }).required("Field is required!"),
-    email: yup.string().email("Please, enter a valid email!").required("Field is required!").required("Field is required!"),
+    email: yup.string().matches(emailRegex, {message: "Please, enter a valid email!"}).required("Field is required!"),
     phone: yup.string().matches(phoneRegex, { message: "Please, enter a valid phone number!" }).required("Field is required!"),
     address: yup.string().required("Field is required!")
 });
